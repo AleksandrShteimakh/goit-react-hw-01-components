cf8a9bc16802be70724a68588567b8fbe85131ec
AleksandrShteimakh <107569995+AleksandrShteimakh@users.noreply.github.com>
2023-01-02 18:01:57 +0100

    render all HTML

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,8 @@
+export default function FriendList(props) {
+  // const { } = props;
+  return (
+    <ul class="friend-list">
+   Произвольное кол-во FriendListItem 
+</ul>
+  )
+};
---
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -0,0 +1,10 @@
+export default function FriendListItem(props) {
+  // const { } = props;
+  return (
+<li class="item">
+  <span class="status"></span>
+  <img class="avatar" src="" alt="User avatar" width="48" />
+  <p class="name"></p>
+</li>
+  )
+};
